0e6da2c127bd663cacccdb90eecebb0708bdb7a6
DmitryShishlov <82105304+DmitryShishlov@users.noreply.github.com>
2022-06-24 18:55:25 +0300

    Add files via upload

---
+++ steam_parcer/content_randomgame.py
@@ -0,0 +1,10 @@
+content_type = {
+    'Популярные новинки': 'https://store.steampowered.com/contenthub/querypaginated/freetoplay/NewReleases/render'
+                          '/?query=&start=+&count=15&cc=RU&l=russian&v=4&tag=',
+    'Лидеры продаж': 'https://store.steampowered.com/contenthub/querypaginated/freetoplay/TopSellers/render/?query'
+                     '=&start=+&count=15&cc=RU&l=russian&v=4&tag=',
+    'Во что играют другие': 'https://store.steampowered.com/contenthub/querypaginated/freetoplay/ConcurrentUsers'
+                            '/render/?query=&start=+&count=15&cc=RU&l=russian&v=4&tag=',
+    'Будущие продукты': 'https://store.steampowered.com/contenthub/querypaginated/freetoplay/ComingSoon/render/?query'
+                        '=&start=+&count=15&cc=RU&l=russian&v=4&tag='
+}
